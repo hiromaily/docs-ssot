53568457b431561aac807ae67d1c3d6909dabdaa
HirokiYasui <hiromaily2@gmail.com>
2026-04-07 21:09:18 +0900

    tweak for vitepress

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
+  base: "/docs-ssot/",
   title: "docs-ssot",
   description: "Documentation SSOT Generator",
   themeConfig: {
